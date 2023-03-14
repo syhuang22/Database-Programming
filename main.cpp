e0edf38421241ec5abec09a23ac04428e420a9e4
James Huang <sh694@duke.edu>
2023-03-14 18:49:53 -0400

    done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,219 @@
 #include <iostream>
 #include <pqxx/pqxx>
+#include <fstream>     // for reading from file
 
 #include "exerciser.h"
 
 using namespace std;
 using namespace pqxx;
+
+void dropTables(connection *C) {
+  try {
+    work W(*C);
+    W.exec("DROP TABLE IF EXISTS PLAYER");
+    W.exec("DROP TABLE IF EXISTS TEAM");
+    W.exec("DROP TABLE IF EXISTS STATE");
+    W.exec("DROP TABLE IF EXISTS COLOR");
+    W.commit();
+  } catch (const std::exception &e) {
+    cerr << e.what() << std::endl;
+  }
+}
+
+void createPlayerTable(connection *C) {
+  try {
+    work W(*C);
+    // Create PLAYER table
+    string sql = "CREATE TABLE PLAYER ("
+                "PLAYER_ID SERIAL PRIMARY KEY,"
+                "TEAM_ID INTEGER NOT NULL REFERENCES TEAM(TEAM_ID),"
+                "UNIFORM_NUM INTEGER NOT NULL,"
+                "FIRST_NAME TEXT NOT NULL,"
+                "LAST_NAME TEXT NOT NULL,"
+                "MPG REAL NOT NULL,"
+                "PPG REAL NOT NULL,"
+                "RPG REAL NOT NULL,"
+                "APG REAL NOT NULL,"
+                "SPG REAL NOT NULL,"
+                "BPG REAL NOT NULL"
+                ");";
+    // Execute the SQL statement
+    W.exec(sql);
+    W.commit();
+    cout << "PLAYER table created successfully." << endl;
+  } catch (const std::exception &e) {
+    cerr << e.what() << std::endl;
+  }
+}
+
+void createTeamTable(connection *C) {
+    try {
+      // Create a new SQL statement to create a team table
+      work W(*C);
+      string sql = "CREATE TABLE TEAM("
+                  "TEAM_ID SERIAL PRIMARY KEY,"
+                  "NAME TEXT NOT NULL,"
+                  "STATE_ID INTEGER NOT NULL,"
+                  "COLOR_ID INTEGER NOT NULL,"
+                  "WINS INTEGER NOT NULL,"
+                  "LOSSES INTEGER NOT NULL"
+                  ");";
+      W.exec(sql);
+      W.commit();
+
+      cout << "Team table created successfully!" << endl;
+    }
+    catch (const std::exception &e) {
+        cerr << e.what() << endl;
+        cerr << "Failed to create team table." << endl;
+    }
+}
+
+void createStateTable(connection *C) {
+    try {
+      // Create a new SQL statement to create the STATE table
+      work W(*C);
+      string sql = "CREATE TABLE STATE("
+                    "STATE_ID SERIAL PRIMARY KEY,"
+                    "NAME TEXT NOT NULL);";
+
+      // Execute the SQL statement
+      W.exec(sql);
+      W.commit();
+
+      cout << "STATE table created successfully." << endl;
+    }
+    catch (const std::exception &e) {
+        cerr << e.what() << endl;
+        cerr << "Failed to create STATE table." << endl;
+    }
+}
+
+void createColorTable(connection *C) {
+    try {
+        // Create a new SQL statement to create the table
+        work W(*C);
+        string sql = "CREATE TABLE COLOR("
+                    "COLOR_ID SERIAL PRIMARY KEY,"
+                    "NAME TEXT NOT NULL);";
+        W.exec(sql);
+        W.commit();
+        
+        cout << "COLOR Table created successfully." << endl;
+    }
+    catch (const std::exception &e) {
+        cerr << e.what() << endl;
+        cerr << "Failed to create COLOR table." << endl;
+    }
+}
+
+
+void loadPlayerData(connection *C) {
+    try {
+        // Read data from file
+        ifstream file("player.txt");
+        if (!file.is_open()) {
+            cerr << "Failed to open player.txt" << endl;
+            return;
+        }
+
+        string line;
+        while (getline(file, line)) {
+            stringstream ss(line);
+            int key_id, team_id, uniform_num, mpg, ppg, rpg, apg;
+            double spg, bpg;
+            string first_name, last_name;
+
+            ss >> key_id >> team_id >> uniform_num >> first_name >> last_name >> mpg >> ppg >> rpg >> apg >> spg >> bpg;
+
+            // Insert player into the database
+            add_player(C, team_id, uniform_num, first_name, last_name, mpg, ppg, rpg, apg, spg, bpg);
+        }
+
+        cout << "Data loaded into PLAYER table successfully." << endl;
+    } catch (const std::exception &e) {
+        cerr << e.what() << endl;
+        cerr << "Failed to load data into PLAYER table." << endl;
+    }
+}
+
+void loadTeamData(connection *C) {
+    try {
+        // Read data from file
+        ifstream file("team.txt");
+        if (!file.is_open()) {
+            cerr << "Failed to open team.txt" << endl;
+            return;
+        }
+
+        string line;
+        while (getline(file, line)) {
+            stringstream ss(line);
+            int team_id, state_id, color_id, wins, losses;
+            string name;
+            ss >> team_id >> name >> state_id >> color_id >> wins >> losses;
+
+            // Insert team into the database
+            add_team(C, name, state_id, color_id, wins, losses);
+        }
+
+        cout << "Data loaded into TEAM table successfully." << endl;
+    } catch (const std::exception &e) {
+        cerr << e.what() << endl;
+        cerr << "Failed to load data into TEAM table." << endl;
+    }
+}
+
+void loadStateData(connection *C) {
+    try {
+      // Read data from file
+      ifstream file("state.txt");
+      if (!file.is_open()) {
+          cerr << "Failed to open state.txt" << endl;
+          return;
+      }
+      string line;
+      while (getline(file, line)) {
+          stringstream ss(line);
+          int state_id;
+          string name;
+          ss >> state_id >> name;
+          // Insert state into the database
+          add_state(C, name);
+      }
+
+      cout << "Data loaded into STATE table successfully." << endl;
+    } catch (const std::exception &e) {
+        cerr << e.what() << endl;
+        cerr << "Failed to load data into STATE table." << endl;
+    }
+}
+
+void loadColorData(connection *C) {
+    try {
+      // Read data from file
+      ifstream file("color.txt");
+      if (!file.is_open()) {
+          cerr << "Failed to open color.txt" << endl;
+          return;
+      }
+      string line;
+      while (getline(file, line)) {
+          stringstream ss(line);
+          int color_id;
+          string color_name;
+
+          ss >> color_id >> color_name;
+
+          // Insert color into the database
+          add_color(C, color_name);
+      }
+      cout << "Data loaded into COLOR table successfully." << endl;
+    } catch (const std::exception &e) {
+        cerr << e.what() << endl;
+        cerr << "Failed to load data into COLOR table." << endl;
+    }
+}
 
 int main (int argc, char *argv[]) 
 {
@@ -31,8 +240,24 @@ int main (int argc, char *argv[])
   //TODO: create PLAYER, TEAM, STATE, and COLOR tables in the ACC_BBALL database
   //      load each table with rows from the provided source txt files
 
+  //drop any tables if exits 
+  dropTables(C);
 
+  //create table needed 
+  createStateTable(C);
+  createColorTable(C);
+  createTeamTable(C);
+  createPlayerTable(C);
+ 
+  //load data 
+  loadStateData(C);
+  loadColorData(C);
+  loadTeamData(C);
+  loadPlayerData(C);
+
+  cout << "testing query" <<endl;
   exercise(C);
+  cout << "end of test" <<endl;
 
 
   //Close database connection
--- query_funcs.cpp
+++ query_funcs.cpp
@@ -1,14 +1,25 @@
 #include "query_funcs.h"
 
 
-void add_player(connection *C, int team_id, int jersey_num, string first_name, string last_name,
+void add_player(connection *C, int team_id, int uniform_num, string first_name, string last_name,
                 int mpg, int ppg, int rpg, int apg, double spg, double bpg) {
     try {
+
+        // cout << "this is team_id:" << team_id << endl;
+        // cout << "this is unifrom_num:" << uniform_num << endl;
+        // cout << "this is first_name:" << first_name << endl;
+        // cout << "this is last_name:" << last_name << endl;
+        // cout << "this is mpg:" << mpg << endl;
+        // cout << "this is ppg:" << ppg << endl;
+        // cout << "this is rpg:" << rpg << endl;
+        // cout << "this is apg:" << apg << endl;
+        // cout << "this is spg:" << spg << endl;
+        // cout << "this is bpg:" << bpg << endl;
         // Create a new SQL statement to insert a new player
         work W(*C);
         stringstream ss;
-        ss << "INSERT INTO PLAYER (team_id, jersey_num, first_name, last_name, mpg, ppg, rpg, apg, spg, bpg) "
-           << "VALUES (" << team_id << ", " << jersey_num << ", " << W.quote(first_name) << ", "
+        ss << "INSERT INTO PLAYER (team_id, uniform_num, first_name, last_name, mpg, ppg, rpg, apg, spg, bpg) "
+           << "VALUES (" << team_id << ", " << uniform_num << ", " << W.quote(first_name) << ", "
            << W.quote(last_name) << ", " << mpg << ", " << ppg << ", " << rpg << ", " << apg << ", "
            << spg << ", " << bpg << ");";
         
@@ -19,6 +30,7 @@ void add_player(connection *C, int team_id, int jersey_num, string first_name, s
     catch (const std::exception &e) {
         cerr << e.what() << endl;
         cerr << "Failed to add player." << endl;
+        exit(1);
     }
 }
 
@@ -92,54 +104,58 @@ void query1(connection *C,
             int use_bpg, double min_bpg, double max_bpg
             ) {
     try {
-        work W(*C);
-        stringstream ss;
-        ss << "SELECT * FROM PLAYER WHERE ";
-        bool first_condition = true;
+        // Start a transaction
+        nontransaction N(*C);
+
+        // Build the SQL query
+        string sql = "SELECT * FROM player WHERE true";
+
         if (use_mpg) {
-            ss << "mpg BETWEEN " << min_mpg << " AND " << max_mpg;
-            first_condition = false;
+            sql += " AND mpg >= " + to_string(min_mpg) + " AND mpg <= " + to_string(max_mpg);
         }
+
         if (use_ppg) {
-            if (!first_condition) {
-                ss << " AND ";
-            }
-            ss << "ppg BETWEEN " << min_ppg << " AND " << max_ppg;
-            first_condition = false;
+            sql += " AND ppg >= " + to_string(min_ppg) + " AND ppg <= " + to_string(max_ppg);
         }
+
         if (use_rpg) {
-            if (!first_condition) {
-                ss << " AND ";
-            }
-            ss << "rpg BETWEEN " << min_rpg << " AND " << max_rpg;
-            first_condition = false;
+            sql += " AND rpg >= " + to_string(min_rpg) + " AND rpg <= " + to_string(max_rpg);
         }
+
         if (use_apg) {
-            if (!first_condition) {
-                ss << " AND ";
-            }
-            ss << "apg BETWEEN " << min_apg << " AND " << max_apg;
-            first_condition = false;
+            sql += " AND apg >= " + to_string(min_apg) + " AND apg <= " + to_string(max_apg);
         }
+
         if (use_spg) {
-            if (!first_condition) {
-                ss << " AND ";
-            }
-            ss << "spg BETWEEN " << min_spg << " AND " << max_spg;
-            first_condition = false;
+            sql += " AND spg >= " + to_string(min_spg) + " AND spg <= " + to_string(max_spg);
         }
+
         if (use_bpg) {
-            if (!first_condition) {
-                ss << " AND ";
-            }
-            ss << "bpg BETWEEN " << min_bpg << " AND " << max_bpg;
+            sql += " AND bpg >= " + to_string(min_bpg) + " AND bpg <= " + to_string(max_bpg);
         }
-        ss << ";";
-        W.exec(ss.str());
-        cout << "Query successfully executed" << endl;
-    }
-    catch (const std::exception &e) {
+
+        // Execute the SQL query
+        result R(N.exec(sql));
+
+        // Print the results
+        cout << "PLAYER_ID TEAM_ID UNIFORM_NUM FIRST_NAME LAST_NAME MPG PPG RPG APG SPG BPG" << endl;
+        for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+            cout << c[0].as<int>() << " "
+                 << c[1].as<int>() << " "
+                 << c[2].as<int>() << " "
+                 << c[3].as<string>() << " "
+                 << c[4].as<string>() << " "
+                 << c[5].as<int>() << " "
+                 << c[6].as<int>() << " "
+                 << c[7].as<int>() << " "
+                 << c[8].as<int>() << " "
+                 << c[9].as<double>() << " "
+                 << c[10].as<double>() << " " << endl;
+        }
+
+    } catch (const exception &e) {
         cerr << e.what() << endl;
+        cerr << "Failed to query players." << endl;
     }
 
 }
@@ -149,18 +165,16 @@ void query2(connection *C, string team_color){
     try {
         nontransaction N(*C);
 
-        work W(*C);
         string sql = "SELECT team.name "
                  "FROM team "
                  "JOIN color ON team.color_id = color.color_id "
-                 "WHERE color.name = " + W.quote(team_color);
-        W.commit();
+                 "WHERE color.name = " + N.quote(team_color);
 
         // Execute SQL query
         result R(N.exec(sql));
 
         // Print out result
-        cout << "Query result:" << endl;
+        cout << "NAME" << endl;
         for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
         cout << c[0].as<string>() << endl;
         }
@@ -173,23 +187,27 @@ void query2(connection *C, string team_color){
 
 void query3(connection *C, string team_name) {
     try {
-        nontransaction N(*C);
-
+        // Start a transaction
         work W(*C);
-        string sql = "SELECT * "
+
+        // Construct the SQL query string
+        string sql = "SELECT player.first_name, player.last_name "
                      "FROM player "
                      "JOIN team ON player.team_id = team.team_id "
-                     "WHERE team.name = " + W.quote(team_name) + ";";
-        W.commit();
+                     "WHERE team.name = " + W.quote(team_name) + " "
+                     "ORDER BY player.ppg DESC;";
 
-        // Execute SQL query
-        result R(N.exec(sql));
+        // Execute the SQL query
+        result R(W.exec(sql));
 
         // Print out result
-        cout << "Query result:" << endl;
+        cout << "FIRST_NAME LAST_NAME" << endl;
         for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-            cout << "Player " << c[1].as<string>() << " " << c[2].as<string>() << endl;
+            cout << c[0].as<string>() << " " << c[1].as<string>() << endl;
         }
+
+        // Commit the transaction
+        W.commit();
     } catch (const exception &e) {
         cerr << e.what() << endl;
         cerr << "Failed to query players by team name." << endl;
@@ -201,50 +219,51 @@ void query4(connection *C, string team_state, string team_color) {
     try {
         nontransaction N(*C);
 
-        work W(*C);
-        stringstream ss;
-        ss << "SELECT TEAM.NAME FROM TEAM, COLOR, STATE WHERE "
-        "TEAM.COLOR_ID = COLOR.COLOR_ID AND "
-        "TEAM.STATE_ID = STATE.STATE_ID AND "
-        "STATE.NAME = "
-        << W.quote(team_state) << " AND "
-        "COLOR.NAME = "
-        << W.quote(team_color) << ";";
-        W.commit();
+        string sql = "SELECT p.uniform_num, p.first_name, p.last_name "
+                     "FROM player AS p "
+                     "JOIN team AS t ON p.team_id = t.team_id "
+                     "JOIN color AS c ON t.color_id = c.color_id "
+                     "JOIN state AS s ON t.state_id = s.state_id "
+                     "WHERE s.name = " + N.quote(team_state) +
+                     " AND c.name = " + N.quote(team_color) + ";";
 
         // Execute SQL query
-        result R(N.exec(ss));
+        result R(N.exec(sql));
 
         // Print out result
-        cout << "Query result:" << endl;
+        cout << "UNIFORM_NUM FIRST_NAME LAST_NAME" << endl;
         for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-            cout << c[0].as<string>() << endl;
+            cout << c[0].as<string>() << " " << c[1].as<string>() << " " << c[2].as<string>() << endl;
         }
     } catch (const exception &e) {
         cerr << e.what() << endl;
-        cerr << "Failed to query team by state and color." << endl;
+        cerr << "Failed to query players by team state and color." << endl;
     }
 }
 
 
 void query5(connection *C, int num_wins) {
     try {
-        work W(*C);
-        stringstream ss;
-        ss << "SELECT TEAM.NAME, WINS FROM TEAM WHERE WINS > " << num_wins << ";";
-        W.commit();
         nontransaction N(*C);
 
+        string sql = "SELECT player.first_name, player.last_name, team.name, team.wins "
+                     "FROM player "
+                     "JOIN team ON player.team_id = team.team_id "
+                     "WHERE team.wins > " + to_string(num_wins) + ";";
+
         // Execute SQL query
-        result R(N.exec(ss));
+        result R(N.exec(sql));
 
         // Print out result
-        cout << "Query result:" << endl;
+        cout << "FIRST_NAME LAST_NAME TEAM_NAME WINS" << endl;
         for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-            cout << c[0].as<string>() << " " << c[1].as<int>() << endl;
+            cout << c[0].as<string>() << " " 
+                 << c[1].as<string>() << " "
+                 << c[2].as<string>() << " "
+                 << c[3].as<int>() << endl;
         }
     } catch (const exception &e) {
         cerr << e.what() << endl;
-        cerr << "Failed to query teams by number of wins." << endl;
+        cerr << "Failed to query players and teams by number of wins." << endl;
     }
 }
